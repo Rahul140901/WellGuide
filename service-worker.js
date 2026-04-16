bb17042c8adbab80856974bb2c41a9ae9b50fcab
rahul_shewale <rahulshewalepatil500@gmail.com>
2026-04-16 19:07:42 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Service Worker for MediGuide PWA
-const CACHE_NAME = 'mediguide-cache-v1';
+// Service Worker for WellGuide PWA
+const CACHE_NAME = 'WellGuide-cache-v1';
 const urlsToCache = [
     '/', // Caches the root, which typically resolves to index.html
     'index.html',
